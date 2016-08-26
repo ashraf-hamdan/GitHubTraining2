e0bc93367d3b46d68242a9020582074d7db3e8f9
ashraf-hamdan <you@example.com>
2016-08-26 02:17:49 -0700

    add Login Screen

--- gen/net/ashraf/saveyourmony/R.java
+++ gen/net/ashraf/saveyourmony/R.java
@@ -31,16 +31,18 @@ public final class R {
         public static final int activity_add_user=0x7f030001;
         public static final int activity_home_screen=0x7f030002;
         public static final int activity_income_list=0x7f030003;
-        public static final int activity_main=0x7f030004;
-        public static final int activity_outlay_list=0x7f030005;
+        public static final int activity_login_screen=0x7f030004;
+        public static final int activity_main=0x7f030005;
+        public static final int activity_outlay_list=0x7f030006;
     }
     public static final class menu {
         public static final int add_outlay=0x7f070000;
         public static final int add_user=0x7f070001;
         public static final int home_screen=0x7f070002;
         public static final int income_list=0x7f070003;
-        public static final int main=0x7f070004;
-        public static final int outlay_list=0x7f070005;
+        public static final int login_screen=0x7f070004;
+        public static final int main=0x7f070005;
+        public static final int outlay_list=0x7f070006;
     }
     public static final class string {
         public static final int action_settings=0x7f050001;
@@ -50,6 +52,7 @@ public final class R {
         public static final int title_activity_add_user=0x7f050004;
         public static final int title_activity_home_screen=0x7f050003;
         public static final int title_activity_income_list=0x7f050007;
+        public static final int title_activity_login_screen=0x7f050008;
         public static final int title_activity_outlay_list=0x7f050006;
     }
     public static final class style {
---
+++ src/net/ashraf/saveyourmony/LoginScreen.java
@@ -0,0 +1,22 @@
+package net.ashraf.saveyourmony;
+
+import android.os.Bundle;
+import android.app.Activity;
+import android.view.Menu;
+
+public class LoginScreen extends Activity {
+
+	@Override
+	protected void onCreate(Bundle savedInstanceState) {
+		super.onCreate(savedInstanceState);
+		setContentView(R.layout.activity_login_screen);
+	}
+
+	@Override
+	public boolean onCreateOptionsMenu(Menu menu) {
+		// Inflate the menu; this adds items to the action bar if it is present.
+		getMenuInflater().inflate(R.menu.login_screen, menu);
+		return true;
+	}
+
+}
