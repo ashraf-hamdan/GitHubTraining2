5e44a8823979fbc278360b685b1052f8afaae180
ashraf-hamdan <you@example.com>
2016-08-28 12:28:45 -0700

    upDate

--- gen/net/ashraf/saveyourmony/R.java
+++ gen/net/ashraf/saveyourmony/R.java
@@ -24,7 +24,25 @@ public final class R {
         public static final int ic_launcher=0x7f020000;
     }
     public static final class id {
-        public static final int action_settings=0x7f080000;
+        public static final int Texpenses=0x7f080003;
+        public static final int action_settings=0x7f080012;
+        public static final int btn_add=0x7f080002;
+        public static final int connect_help=0x7f080008;
+        public static final int empty=0x7f08000c;
+        public static final int income=0x7f080005;
+        public static final int line=0x7f080007;
+        public static final int listview=0x7f08000b;
+        public static final int outlay_name=0x7f080000;
+        public static final int pager=0x7f080011;
+        public static final int result=0x7f080009;
+        public static final int textView1=0x7f08000d;
+        public static final int textView2=0x7f08000e;
+        public static final int textView3=0x7f08000f;
+        public static final int textView4=0x7f080010;
+        public static final int value=0x7f080001;
+        public static final int value_Texpenses=0x7f080004;
+        public static final int value_income=0x7f080006;
+        public static final int value_result=0x7f08000a;
     }
     public static final class layout {
         public static final int activity_add_outlay=0x7f030000;
@@ -34,6 +52,8 @@ public final class R {
         public static final int activity_login_screen=0x7f030004;
         public static final int activity_main=0x7f030005;
         public static final int activity_outlay_list=0x7f030006;
+        public static final int item=0x7f030007;
+        public static final int services=0x7f030008;
     }
     public static final class menu {
         public static final int add_outlay=0x7f070000;
